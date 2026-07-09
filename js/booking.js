f5c6e75fe124912520382b72aefc5d49f6e462be
Ananyajain002255 <ananyajain0074@gmail.com>
2026-07-09 17:53:52 +0530

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -1,1 +1,51 @@
+const seatsContainer = document.getElementById("seats");
+const count = document.getElementById("count");
+const price = document.getElementById("price");
 
+const seatPrice = 500;
+
+for(let i=1;i<=48;i++){
+
+    const seat=document.createElement("div");
+
+    seat.classList.add("seat");
+
+    seat.innerHTML=i;
+
+    seat.addEventListener("click",()=>{
+
+        seat.classList.toggle("selected");
+
+        updateBooking();
+
+    });
+
+    seatsContainer.appendChild(seat);
+
+}
+
+function updateBooking(){
+
+    const selected=document.querySelectorAll(".selected");
+
+    count.innerHTML=selected.length;
+
+    price.innerHTML=selected.length*seatPrice;
+
+}
+
+document.getElementById("bookBtn").addEventListener("click",()=>{
+
+    if(count.innerHTML==0){
+
+        alert("Please Select At Least One Seat");
+
+    }
+
+    else{
+
+        alert("Booking Successful!");
+
+    }
+
+});
