95f8133cdb68f1a8696940ccac1305726a94bbce
Ananyajain002255 <ananyajain0074@gmail.com>
2026-07-09 17:48:11 +0530

    Update signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,22 @@
+const form = document.getElementById("signupForm");
 
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+const password = document.getElementById("password").value;
+const confirm = document.getElementById("confirm").value;
+
+if(password !== confirm){
+
+alert("Passwords do not match!");
+
+return;
+
+}
+
+alert("Account Created Successfully!");
+
+window.location.href="login.html";
+
+});
