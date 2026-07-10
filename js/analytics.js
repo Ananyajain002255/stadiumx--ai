fabbd681a2b7a0915feb3d6bace179b813c05b56
Ananyajain002255 <ananyajain0074@gmail.com>
2026-07-10 20:40:44 +0530

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,1 +1,15 @@
+const totalTickets = Number(localStorage.getItem("totalTickets")) || 0;
 
+const totalRevenue = Number(localStorage.getItem("totalRevenue")) || 0;
+
+const totalTournaments = 8;
+const totalTeams = 24;
+
+
+document.getElementById("ticketCount").innerHTML = totalTickets;
+
+document.getElementById("revenue").innerHTML = totalRevenue;
+
+document.getElementById("tournamentCount").innerHTML = totalTournaments;
+
+document.getElementById("teamCount").innerHTML = totalTeams;
