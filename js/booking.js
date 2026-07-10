343911183974a5fb312f581cf91dcd0aa6b29973
Ananyajain002255 <ananyajain0074@gmail.com>
2026-07-10 18:03:10 +0530

    Update booking.js

--- js/booking.js
+++ js/booking.js
@@ -262,15 +262,23 @@ document.addEventListener("keydown",function(e){
 
 const themeBtn = document.getElementById("themeBtn");
 
-themeBtn.addEventListener("click",function(){
+// Page load hone par saved theme apply karo
+if(localStorage.getItem("theme") === "light"){
+    document.body.classList.add("light");
+    themeBtn.innerHTML = "☀ Light Mode";
+}
+
+themeBtn.addEventListener("click", function(){
 
     document.body.classList.toggle("light");
 
     if(document.body.classList.contains("light")){
-        themeBtn.innerHTML="☀ Light Mode";
+        themeBtn.innerHTML = "☀ Light Mode";
+        localStorage.setItem("theme","light");
     }
     else{
-        themeBtn.innerHTML="🌙 Dark Mode";
+        themeBtn.innerHTML = "🌙 Dark Mode";
+        localStorage.setItem("theme","dark");
     }
 
 });
