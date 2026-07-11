0493cda451220c8f676d519d3bca00f0a7c10f59
Ananyajain002255 <ananyajain0074@gmail.com>
2026-07-11 15:51:47 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,24 @@
 console.log("Dashboard Loaded Successfully");
+
+const themeBtn = document.getElementById("themeBtn");
+
+// Page load hone par saved theme apply karo
+if(localStorage.getItem("theme") === "light"){
+    document.body.classList.add("light");
+    themeBtn.innerHTML = "☀ Light Mode";
+}
+
+themeBtn.addEventListener("click", function(){
+
+    document.body.classList.toggle("light");
+
+    if(document.body.classList.contains("light")){
+        themeBtn.innerHTML = "☀ Light Mode";
+        localStorage.setItem("theme","light");
+    }
+    else{
+        themeBtn.innerHTML = "🌙 Dark Mode";
+        localStorage.setItem("theme","dark");
+    }
+
+});
