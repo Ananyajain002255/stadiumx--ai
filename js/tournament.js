5a37712ac54357ccb4038daaa1e60c37b52cc05b
Ananyajain002255 <ananyajain0074@gmail.com>
2026-07-11 14:59:07 +0530

    Update tournament.js

--- js/tournament.js
+++ js/tournament.js
@@ -123,3 +123,26 @@ function deleteTournament(index){
     }
 
 }
+
+const themeBtn = document.getElementById("themeBtn");
+
+// Page load hone par saved theme apply karo
+if(localStorage.getItem("theme") === "light"){
+    document.body.classList.add("light");
+    themeBtn.innerHTML = "☀ Light Mode";
+}
+
+themeBtn.addEventListener("click", function(){
+
+    document.body.classList.toggle("light");
+
+    if(document.body.classList.contains("light")){
+        themeBtn.innerHTML = "☀ Light Mode";
+        localStorage.setItem("theme","light");
+    }
+    else{
+        themeBtn.innerHTML = "🌙 Dark Mode";
+        localStorage.setItem("theme","dark");
+    }
+
+});
